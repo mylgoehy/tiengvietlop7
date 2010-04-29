5cc93a6675b0d650ea76525bcb9fa999c80d9df0
nghai89 <nghai89@56fbe389-1366-c183-eb61-de4e928d7dbb>
2010-04-29 04:49:05 +0000

    Giao dien ver1.0 ( chua hoan thien ) 0712143-0712157

---
+++ trunk/tiengvietlop7/Source code/Temp/Temp/PageSwitcher.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Temp
+{
+    public partial class PageSwitcher : UserControl
+    {
+        public PageSwitcher()
+        {
+            InitializeComponent();
+        }
+        // 1st overload
+      public void Navigate( UserControl nextPage )
+      {
+         this.Content = nextPage;
+      }
+ 
+       // 2nd overload
+      public void Navigate(UserControl nextPage, object state)
+      {
+          this.Content = nextPage;
+          ISwitchable s = nextPage as ISwitchable;
+          if (s != null)
+          {
+              s.UtilizeState(state);
+          }
+          else
+          {
+              throw new ArgumentException("nextPage is not ISwitchable! " + nextPage.Name.ToString());
+          }
+      }
+    }
+}
