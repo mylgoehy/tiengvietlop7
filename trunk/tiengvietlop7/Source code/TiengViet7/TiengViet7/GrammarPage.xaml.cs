ccc4b8a93161f4264ad1d2ed36ecd27d91c5a578
nguyenhung138 <nguyenhung138@56fbe389-1366-c183-eb61-de4e928d7dbb>
2010-05-11 18:14:08 +0000

    0712157 - Thiet ke giao dien

---
+++ trunk/tiengvietlop7/Source code/TiengViet7/TiengViet7/GrammarPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace TiengViet7
+{
+    /// <summary>
+    /// Interaction logic for GrammarPage.xaml
+    /// </summary>
+    public partial class GrammarPage : Window
+    {
+        public GrammarPage()
+        {
+            InitializeComponent();
+        }
+
+        double _marqueeTimeInSeconds = 7;
+
+        private void LeftToRightMarquee()
+        {
+            double height = canMain.ActualHeight - tbmarquee.ActualHeight;
+            tbmarquee.Margin = new Thickness(0, height / 2, 0, 0);
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = -tbmarquee.ActualWidth;
+            doubleAnimation.To = canMain.ActualWidth;
+            doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(_marqueeTimeInSeconds));
+            tbmarquee.BeginAnimation(Canvas.LeftProperty, doubleAnimation);
+        }
+
+        private void GrammarPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            LeftToRightMarquee();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ trunk/tiengvietlop7/Source code/TiengViet7/TiengViet7/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace TiengViet7
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Window
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        double _marqueeTimeInSeconds = 7;
+
+        private void LeftToRightMarquee()
+        {
+            double height = canMain.ActualHeight - tbmarquee.ActualHeight;
+            tbmarquee.Margin = new Thickness(0, height / 2, 0, 0);
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = -tbmarquee.ActualWidth;
+            doubleAnimation.To = canMain.ActualWidth;
+            doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(_marqueeTimeInSeconds));
+            tbmarquee.BeginAnimation(Canvas.LeftProperty, doubleAnimation);
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            LeftToRightMarquee();
+        }
+
+        private void GrammarButton_Click(object sender, RoutedEventArgs e)
+        {
+            GrammarPage grammarPage = new GrammarPage();
+            grammarPage.Show();
+        }
+
+        private void LessonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CompositionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReadMoreButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
